49445d6632c1efeadb5f4d8da0ad09b677305c49
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-26 14:47:46 -0400

    Update User.js
    set up User model to take username, password, and accessLevel for use for restricting access to changing the database in future code

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../database/connection");
+
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  accessLevel: {
+    type: DataTypes.ENUM("admin", "helper"),
+    defaultValue: "helper",
+    allowNull: false,
+  },
+});
+
+// Hash the password before saving to the database
+User.beforeCreate(async (user) => {
+  const saltRounds = 10;
+  user.password = await bcrypt.hash(user.password, saltRounds);
+});
+
+module.exports = User;
