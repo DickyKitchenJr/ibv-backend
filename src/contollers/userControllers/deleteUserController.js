4f24f57a78c5409aea3f04fd03861b08825f3a62
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-27 14:36:16 -0400

    Create deleteUserController.js
    added controller for handling deleting users from the database

---
+++ src/contollers/userControllers/deleteUserController.js
@@ -0,0 +1,24 @@
+const User = require("../../models/User");
+
+const deleteUserController = async (req, res) => {
+  try {
+    const usernameToDelete = req.params.username;
+    // Check if the user exists
+    const userToDelete = await User.findOne({
+      where: { username: usernameToDelete },
+    });
+    if (!userToDelete) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    // Delete the user
+    await userToDelete.destroy();
+    // Respond with a success message
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    // Handle specific error cases if needed
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = deleteUserController;
