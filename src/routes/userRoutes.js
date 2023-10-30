c7df387a831374bfacca2eb8b21de49e2b5bc8a4
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-30 15:35:21 -0400

    Update userRoutes.js
    added the appropriate controllers to the get and put methods

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const addUserController = require("../controllers/userControllers/addUserController");
 const deleteUserController = require("../controllers/userControllers/deleteUserController");
+const getUserController = require("../controllers/userControllers/getUserController");
+const modifyUserController = require("../controllers/userControllers/modifyUserController");
 
-
+router.get('/users/:username', getUserController)
 router.post("/users", addUserController);
+router.put('/users/:username', modifyUserController)
 router.delete("/users/:username", deleteUserController);
 
 module.exports = router;
