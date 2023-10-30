c3fe4113e4c1d06bcec387197c3c09cd94380cf9
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-30 15:34:17 -0400

    Create getUserController.js
    set up get request to return the full list if no name is included, or to return just the specified user if one is in the params

---
+++ src/controllers/userControllers/getUserController.js
@@ -0,0 +1,24 @@
+const User = require("../../models/User");
+
+const getUserController = async (req, res) => {
+  try {
+    const { username } = req.params;
+    // If a username is provided, find a specific user
+    if (username) {
+      const user = await User.findOne({ where: { username } });
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      return res.status(200).json(user);
+    }
+    // If no username is provided, find all users
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error("Error getting users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = getUserController;
