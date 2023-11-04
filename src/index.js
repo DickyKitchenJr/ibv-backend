1e9c67a64d04acb020a56c378e970520fe15a6f6
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-04 18:10:49 -0400

    Update index.js
    added passport, session, and sessionStore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const sequelize = require('./database/connection')
+const express = require("express");
+require("dotenv").config();
+const session = require("express-session");
+const passport = require("./utils/passport");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require("./database/connection");
 const app = express();
-const router = require('./routes/index')
+const router = require("./routes/index");
 const PORT = process.env.PORT || 3030;
+
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+  checkExpirationInterval: 30 * 60 * 1000, //check for expired sessions every 30 min
+  expiration: 24 * 60 * 60 * 1000, // expire sessions after 24 hours
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
+sessionStore.sync();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,8 +37,11 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-  app.use('/', router)
+app.use("/", router);
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
