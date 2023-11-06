ab51d6f9d045132f24e78fad2b97c64288e23b21
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-05 22:29:58 -0500

    Update userRoutes.js
    added middleware to routes requiring admin privileges

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,10 +4,15 @@ const addUserController = require("../controllers/userControllers/addUserControl
 const deleteUserController = require("../controllers/userControllers/deleteUserController");
 const getUserController = require("../controllers/userControllers/getUserController");
 const modifyUserController = require("../controllers/userControllers/modifyUserController");
+const loginController = require("../controllers/userControllers/loginController");
+const passport = require("../utils/passport");
+const isAdmin = require("../utils/isAdmin");
 
-router.get("/:username", getUserController);
-router.post("/", addUserController);
-router.put("/:username", modifyUserController);
-router.delete("/:username", deleteUserController);
+router.post("/login", loginController);
+router.get("/", isAdmin, getUserController);
+router.get("/:username", isAdmin, getUserController);
+router.post("/", isAdmin, addUserController);
+router.put("/:username", isAdmin, modifyUserController);
+router.delete("/:username", isAdmin, deleteUserController);
 
 module.exports = router;
