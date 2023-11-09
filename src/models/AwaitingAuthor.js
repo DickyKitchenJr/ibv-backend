d4de4e4fb737e00d54a511d253d47e777526fae8
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-09 17:11:22 -0500

    Update AwaitingAuthor.js

--- src/models/AwaitingAuthor.js
+++ src/models/AwaitingAuthor.js
@@ -1,5 +1,5 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database/connection');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/connection");
 
 const AwaitingAuthor = sequelize.define("AwaitingAuthor", {
   firstName: {
