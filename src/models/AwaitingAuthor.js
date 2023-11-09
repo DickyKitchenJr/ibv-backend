c0061258a5482f3bbf99996b0d4119a922b97486
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-09 11:51:59 -0500

    Update AwaitingAuthor.js
    added options to the model

--- src/models/AwaitingAuthor.js
+++ src/models/AwaitingAuthor.js
@@ -3,55 +3,67 @@ const sequelize = require('../database/connection');
 
 const AwaitingAuthor = sequelize.define("AwaitingAuthor", {
   firstName: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: false,
   },
   lastName: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: false,
   },
   email: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: false,
   },
   website: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: true,
   },
   umbrellaGenre: {
-    type: DataTypes.ARRAY(DataTypes.STRING), 
+    type: DataTypes.ARRAY(DataTypes.STRING),
     allowNull: false,
   },
   subGenre: {
-    type: DataTypes.ARRAY(DataTypes.STRING), 
+    type: DataTypes.ARRAY(DataTypes.STRING),
     allowNull: false,
   },
   instagram: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: true,
   },
   facebook: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: true,
   },
   twitter: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: true,
   },
   tiktok: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: true,
   },
   goodreads: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
     allowNull: true,
   },
   mastodon: {
-    type: DataTypes.STRING, 
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  amazonBio: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  threads: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  bookbub: {
+    type: DataTypes.STRING,
     allowNull: true,
   },
   bio: {
-    type: DataTypes.TEXT, 
+    type: DataTypes.TEXT,
     allowNull: false,
   },
 });
