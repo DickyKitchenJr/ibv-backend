7b589d1f6e61a929e6b45da8b7fe0f8f8b1f4f8b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-10 13:17:06 -0500

    Update Author.js
    added validation for url inputs

--- src/models/Author.js
+++ src/models/Author.js
@@ -20,6 +20,9 @@ const Author = sequelize.define("Author", {
   website: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   umbrellaGenre: {
     type: DataTypes.ARRAY(DataTypes.STRING),
@@ -32,38 +35,65 @@ const Author = sequelize.define("Author", {
   instagram: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   facebook: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   twitter: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   tiktok: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   goodreads: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   mastodon: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   amazonBio: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   threads: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   bookbub: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isUrl: true,
+    },
   },
   bio: {
     type: DataTypes.TEXT,
@@ -75,5 +105,9 @@ const Author = sequelize.define("Author", {
     defaultValue: false,
   },
 });
+
+// TODO: erase on production
+// opted to use the following as opposed to sequelize-cli for ease of use
+Author.sync();
 
 module.exports = Author;
