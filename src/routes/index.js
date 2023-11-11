40f066c5cd590266fe648c45769204956cdc3f43
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-11 16:50:32 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userRoutes = require('./userRoutes');
+const awaitingAuthorRoutes = require('./awaitingAuthorRoutes');
 
+// TODO: place /users router above /awaitingauthors router before production
+router.use('/awaitingauthor', awaitingAuthorRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
