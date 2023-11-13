8a53baf5b1e8bdcf95c24eaab70ea1c2378c805b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-13 15:53:27 -0500

    Update awaitingAuthorRoutes.js
    added the rest of the routes and modified the put route to accept an email as the parameter

--- src/routes/awaitingAuthorRoutes.js
+++ src/routes/awaitingAuthorRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const addAwaitingAuthorController = require('../controllers/awaitingAuthorControllers/addAwaitingAuthorController')
+const addAwaitingAuthorController = require("../controllers/awaitingAuthorControllers/addAwaitingAuthorController");
+const deleteAwaitingAuthorController = require("../controllers/awaitingAuthorControllers/deleteAwaitingAuthorController");
+const modifyAwaitingAuthorController = require("../controllers/awaitingAuthorControllers/modifyAwaitingAuthor");
+const getAwaitingAuthorsController = require("../controllers/awaitingAuthorControllers/getAwaitingAuthorController");
 
-
-router.post('/', addAwaitingAuthorController);
+router.get("/", getAwaitingAuthorsController);
+router.post("/", addAwaitingAuthorController);
+router.put("/:email", modifyAwaitingAuthorController);
+router.delete("/", deleteAwaitingAuthorController);
 
 module.exports = router;
