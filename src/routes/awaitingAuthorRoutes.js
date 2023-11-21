fb5830396041af6ebfbf139e5a5b3cb9900ee92a
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-21 10:55:31 -0500

    Update awaitingAuthorRoutes.js
    added authentication for routes

--- src/routes/awaitingAuthorRoutes.js
+++ src/routes/awaitingAuthorRoutes.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("../utils/passport");
 const addAwaitingAuthorController = require("../controllers/awaitingAuthorControllers/addAwaitingAuthorController");
 const deleteAwaitingAuthorController = require("../controllers/awaitingAuthorControllers/deleteAwaitingAuthorController");
 const modifyAwaitingAuthorController = require("../controllers/awaitingAuthorControllers/modifyAwaitingAuthor");
 const getAwaitingAuthorsController = require("../controllers/awaitingAuthorControllers/getAwaitingAuthorController");
 
-router.get("/", getAwaitingAuthorsController);
-router.post("/", addAwaitingAuthorController);
-router.put("/:email", modifyAwaitingAuthorController);
-router.delete("/:email", deleteAwaitingAuthorController);
+router.get("/", passport.authenticate("local"), getAwaitingAuthorsController);
+router.post("/", passport.authenticate("local"), addAwaitingAuthorController);
+router.put(
+  "/:email",
+  passport.authenticate("local"),
+  modifyAwaitingAuthorController
+);
+router.delete(
+  "/:email",
+  passport.authenticate("local"),
+  deleteAwaitingAuthorController
+);
 
 module.exports = router;
