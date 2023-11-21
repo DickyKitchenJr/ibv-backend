f3e1aa1cd7073ade7cea26d2b7c742dd3b966d5c
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-21 10:55:22 -0500

    Update authorRoutes.js
    added authentication for routes

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("../utils/passport");
+const isAdmin = require("../utils/isAdmin");
 const deleteAuthorController = require("../controllers/authorControllers/deleteAuthorController");
 const modifyAuthorController = require("../controllers/authorControllers/modifyAuthorController");
 const getAuthorController = require("../controllers/authorControllers/getAuthorController");
@@ -10,13 +12,17 @@ const getUnverifiedAuthorsController = require("../controllers/authorControllers
 //for getting sanitized author data
 router.get("/", getAuthorController);
 //for getting all author data
-router.get("/admin", getAllAuthorController);
+router.get("/admin", isAdmin, getAllAuthorController);
 //for getting an author by email address
-router.get("/admin/:email", getAllAuthorController);
+router.get(
+  "/admin/:email",
+  passport.authenticate("local"),
+  getAllAuthorController
+);
 //for getting authors where isVerified = false
-router.get('/admin/unverified', getUnverifiedAuthorsController);
-router.post("/", addAuthorController);
-router.put("/:email", modifyAuthorController);
-router.delete("/:email", deleteAuthorController);
+router.get("/admin/unverified", isAdmin, getUnverifiedAuthorsController);
+router.post("/", passport.authenticate("local"), addAuthorController);
+router.put("/:email", passport.authenticate("local"), modifyAuthorController);
+router.delete("/:email", isAdmin, deleteAuthorController);
 
 module.exports = router;
