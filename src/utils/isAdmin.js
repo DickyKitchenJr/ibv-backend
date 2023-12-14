4fca10cb9e8f03fa2891b8d098f418b9a6ac2963
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-12-14 15:09:08 -0500

    Update isAdmin.js
    deleted unnecessary import of passport

--- src/utils/isAdmin.js
+++ src/utils/isAdmin.js
@@ -1,8 +1,9 @@
-const passport = require('./passport');
-
 const isAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.accessLevel === "admin") {
     return next();
+  }
+  if(req.isAuthenticated()){
+    return res.status(401).json({ error:"Unauthorized" })
   }
   res.status(403).json({ error: "Forbidden" });
 };
